12d0d2d714ff6492282692066876edd7ea80487e
Kelvin Santos <me@quleuber.dev>
2022-06-24 17:23:02 -0300

    refactor api.rs

--- src/api.rs
+++ src/api.rs
@@ -123,6 +123,12 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::In
 pub fn api_loop(node_query_sender: SyncSender<NodeRequest>) {
   let runtime = tokio::runtime::Runtime::new().unwrap();
 
+  runtime.block_on(async move {
+    api_serve(node_query_sender).await;
+  });
+}
+
+async fn api_serve(node_query_sender: SyncSender<NodeRequest>) {
   async fn ask<T>(
     node_query_tx: SyncSender<NodeRequest>,
     f: impl Fn(oneshot::Sender<T>) -> NodeRequest,
@@ -134,118 +140,116 @@ pub fn api_loop(node_query_sender: SyncSender<NodeRequest>) {
     result
   }
 
-  runtime.block_on(async move {
-    let root = warp::path::end().map(|| "UP");
+  let root = warp::path::end().map(|| "UP");
 
-    // TODO: macro to wrap those clones
+  // TODO: macro to wrap those clones
 
+  let query_tx = node_query_sender.clone();
+  let get_tick = path!("tick").then(move || {
+    let query_tx = query_tx.clone();
+    async move {
+      let tick = ask(query_tx, |tx| NodeRequest::GetTick { tx }).await;
+      ok_json(format!("Tick: {}", tick))
+    }
+  });
+
+  // == Blocks ==
+
+  let query_tx = node_query_sender.clone();
+  let get_blocks = path!("blocks").then(move || {
+    let query_tx = query_tx.clone();
+    async move {
+      let range = (-10, -1);
+      let blocks = ask(query_tx, |tx| NodeRequest::GetBlocks { range, tx }).await;
+      ok_json(blocks)
+    }
+  });
+
+  let get_block = || {
     let query_tx = node_query_sender.clone();
-    let get_tick = path!("tick").then(move || {
+    path!("blocks" / String / ..).and_then(move |hash_hex: String| {
       let query_tx = query_tx.clone();
       async move {
-        let tick = ask(query_tx, |tx| NodeRequest::GetTick { tx }).await;
-        ok_json(format!("Tick: {}", tick))
-      }
-    });
-
-    // == Blocks ==
-
-    let query_tx = node_query_sender.clone();
-    let get_blocks = path!("blocks").then(move || {
-      let query_tx = query_tx.clone();
-      async move {
-        let range = (-10, -1);
-        let blocks = ask(query_tx, |tx| NodeRequest::GetBlocks { range, tx }).await;
-        ok_json(blocks)
-      }
-    });
-
-    let get_block = || {
-      let query_tx = node_query_sender.clone();
-      path!("blocks" / String / ..).and_then(move |hash_hex: String| {
-        let query_tx = query_tx.clone();
-        async move {
-          let hash_hex = hash_hex.strip_prefix("0x").unwrap_or(&hash_hex);
-          match hex_to_u256(hash_hex) {
-            Ok(hash) => {
-              let block = ask(query_tx, |tx| NodeRequest::GetBlock { hash, tx }).await;
-              Ok(block)
-            }
-            Err(err) => {
-              Err(reject::custom(InvalidParameter::from(format!("Invalid block hash: '{}'", err))))
-            }
+        let hash_hex = hash_hex.strip_prefix("0x").unwrap_or(&hash_hex);
+        match hex_to_u256(hash_hex) {
+          Ok(hash) => {
+            let block = ask(query_tx, |tx| NodeRequest::GetBlock { hash, tx }).await;
+            Ok(block)
+          }
+          Err(err) => {
+            Err(reject::custom(InvalidParameter::from(format!("Invalid block hash: '{}'", err))))
           }
         }
-      })
-    };
-
-    let get_block_go = get_block().and(path!()).map(ok_json);
-
-    let blocks_router = get_blocks //
-      .or(get_block_go);
-
-    // == Functions ==
-
-    let query_tx = node_query_sender.clone();
-    let get_functions = path!("functions").then(move || {
-      let query_tx = query_tx.clone();
-      async move {
-        let functions = ask(query_tx, |tx| NodeRequest::GetFunctions { tx }).await;
-        let functions: Vec<u128> = functions.into_iter().map(|x| x as u128).collect();
-        let functions = u128_names_to_strings(&functions);
-        ok_json(functions)
       }
-    });
+    })
+  };
 
-    let get_function_base =
-      path!("functions" / String / ..).and_then(move |name_txt: String| async move {
-        if let Some(name) = name_to_u128_safe(&name_txt) {
-          Ok(name)
-        } else {
-          let msg = format!("Invalid function name: '{}'", name_txt);
-          Err(reject::custom(InvalidParameter::from(msg)))
-        }
-      });
+  let get_block_go = get_block().and(path!()).map(ok_json);
 
-    let query_tx = node_query_sender.clone();
-    let _get_function = get_function_base.and(path!()).then(move |name: u128| {
-      let query_tx = query_tx.clone();
-      async move {
-        let function = ask(query_tx, |tx| NodeRequest::GetFunction { name, tx }).await;
-        ok_json(function)
-      }
-    });
+  let blocks_router = get_blocks //
+    .or(get_block_go);
 
-    let query_tx = node_query_sender.clone();
-    let get_function_state = get_function_base.and(path!("state")).and_then(move |name: u128| {
-      let query_tx = query_tx.clone();
-      async move {
-        let state = ask(query_tx, |tx| NodeRequest::GetState { name, tx }).await;
-        if let Some(state) = state {
-          Ok(ok_json(state))
-        } else {
-          Err(reject::not_found())
-        }
-      }
-    });
+  // == Functions ==
 
-    let functions_router = get_functions //
-      // .or(get_function) //
-      .or(get_function_state);
-
-    // ==
-
-    let app = root.or(get_tick).or(blocks_router).or(functions_router);
-    let app = app.recover(handle_rejection);
-    let app = app.map(|reply| warp::reply::with_header(reply, "Access-Control-Allow-Origin", "*"));
-
-    let listener_v4 = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let listener_v6 = TcpListener::bind("[::1]:8000").await.unwrap();
-    let incoming_connections =
-      TcpListenerStream::new(listener_v4).merge(TcpListenerStream::new(listener_v6));
-
-    warp::serve(app).run_incoming(incoming_connections).await;
+  let query_tx = node_query_sender.clone();
+  let get_functions = path!("functions").then(move || {
+    let query_tx = query_tx.clone();
+    async move {
+      let functions = ask(query_tx, |tx| NodeRequest::GetFunctions { tx }).await;
+      let functions: Vec<u128> = functions.into_iter().map(|x| x as u128).collect();
+      let functions = u128_names_to_strings(&functions);
+      ok_json(functions)
+    }
   });
+
+  let get_function_base =
+    path!("functions" / String / ..).and_then(move |name_txt: String| async move {
+      if let Some(name) = name_to_u128_safe(&name_txt) {
+        Ok(name)
+      } else {
+        let msg = format!("Invalid function name: '{}'", name_txt);
+        Err(reject::custom(InvalidParameter::from(msg)))
+      }
+    });
+
+  let query_tx = node_query_sender.clone();
+  let _get_function = get_function_base.and(path!()).then(move |name: u128| {
+    let query_tx = query_tx.clone();
+    async move {
+      let function = ask(query_tx, |tx| NodeRequest::GetFunction { name, tx }).await;
+      ok_json(function)
+    }
+  });
+
+  let query_tx = node_query_sender.clone();
+  let get_function_state = get_function_base.and(path!("state")).and_then(move |name: u128| {
+    let query_tx = query_tx.clone();
+    async move {
+      let state = ask(query_tx, |tx| NodeRequest::GetState { name, tx }).await;
+      if let Some(state) = state {
+        Ok(ok_json(state))
+      } else {
+        Err(reject::not_found())
+      }
+    }
+  });
+
+  let functions_router = get_functions //
+    // .or(get_function) //
+    .or(get_function_state);
+
+  // ==
+
+  let app = root.or(get_tick).or(blocks_router).or(functions_router);
+  let app = app.recover(handle_rejection);
+  let app = app.map(|reply| warp::reply::with_header(reply, "Access-Control-Allow-Origin", "*"));
+
+  let listener_v4 = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+  let listener_v6 = TcpListener::bind("[::1]:8000").await.unwrap();
+  let listener =
+    TcpListenerStream::new(listener_v4).merge(TcpListenerStream::new(listener_v6));
+
+  warp::serve(app).run_incoming(listener).await;
 }
 
 mod ser {
