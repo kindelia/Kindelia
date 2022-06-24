0ef765e562c3d93b8672004bdedac58cb5e9a3d6
Kelvin Santos <me@quleuber.dev>
2022-06-24 17:07:48 -0300

    refactoring api.rs

--- src/api.rs
+++ src/api.rs
@@ -4,7 +4,6 @@
 #![warn(unused_variables)]
 #![warn(clippy::style)]
 #![allow(clippy::let_and_return)]
-//use std::fmt::format;
 use std::sync::mpsc::SyncSender;
 
 use serde_json::json;
@@ -16,7 +15,7 @@ use warp::reject::{self, Rejection};
 use warp::reply::{self, Reply};
 use warp::{path, Filter};
 
-use crate::hvm::{name_to_u128, u128_to_name};
+use crate::hvm;
 use crate::node::Request as NodeRequest;
 use crate::util::U256;
 
@@ -35,7 +34,7 @@ pub fn hex_to_u256(hex: &str) -> Result<U256, String> {
   let bytes = hex::decode(hex);
   let bytes = match bytes {
     Ok(bytes) => bytes,
-    Err(_) => return Err(format!("Invalid hexadecimal string: {}", hex)),
+    Err(_) => return Err(format!("Invalid hexadecimal string: '{}'", hex)),
   };
   if bytes.len() != 256 / 8 {
     Err(format!("Invalid hexadecimal string: {}", hex))
@@ -43,10 +42,6 @@ pub fn hex_to_u256(hex: &str) -> Result<U256, String> {
     let num = U256::from_big_endian(&bytes);
     Ok(num)
   }
-}
-
-fn u128_names_to_strings(names: &[u128]) -> Vec<String> {
-  names.iter().copied().map(u128_to_name).collect::<Vec<_>>()
 }
 
 fn ok_json<T>(data: T) -> warp::reply::Json
@@ -63,6 +58,33 @@ where
 {
   let json_body = json!({ "status": "error", "error": error });
   warp::reply::json(&json_body)
+}
+
+// HVM
+// ===
+
+pub fn name_to_u128_safe(name: &str) -> Option<u128> {
+  let mut num: u128 = 0;
+  for (i, chr) in name.chars().enumerate() {
+    debug_assert!(i < 20, "Name too big: `{}`.", name);
+    num = (num << 6) + char_to_u128_safe(chr)?;
+  }
+  return Some(num);
+}
+
+pub const fn char_to_u128_safe(chr: char) -> Option<u128> {
+  match chr {
+    '.' => Some(0),
+    '0'..='9' => Some(1 + chr as u128 - '0' as u128),
+    'A'..='Z' => Some(11 + chr as u128 - 'A' as u128),
+    'a'..='z' => Some(37 + chr as u128 - 'a' as u128),
+    '_' => Some(63),
+    _ => None,
+  }
+}
+
+fn u128_names_to_strings(names: &[u128]) -> Vec<String> {
+  names.iter().copied().map(hvm::u128_to_name).collect::<Vec<_>>()
 }
 
 // Erros
@@ -101,53 +123,56 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::In
 pub fn api_loop(node_query_sender: SyncSender<NodeRequest>) {
   let runtime = tokio::runtime::Runtime::new().unwrap();
 
-  runtime.block_on(async move {
-    // // Custom rejection handler that maps rejections into responses.
-    // // https://docs.rs/warp/latest/warp/reject/index.html
+  async fn ask<T>(
+    node_query_tx: SyncSender<NodeRequest>,
+    f: impl Fn(oneshot::Sender<T>) -> NodeRequest,
+  ) -> T {
+    let (tx, rx) = oneshot::channel();
+    let request = f(tx);
+    node_query_tx.send(request).unwrap();
+    let result = rx.await.expect("Node query channel closed");
+    result
+  }
 
+  runtime.block_on(async move {
     let root = warp::path::end().map(|| "UP");
 
     // TODO: macro to wrap those clones
 
-    let node_query_tx = node_query_sender.clone();
+    let query_tx = node_query_sender.clone();
     let get_tick = path!("tick").then(move || {
-      let node_query_tx = node_query_tx.clone();
+      let query_tx = query_tx.clone();
       async move {
-        let (tx, rx) = oneshot::channel();
-        node_query_tx.send(NodeRequest::GetTick { tx }).unwrap();
-        let tick = rx.await.unwrap();
+        let tick = ask(query_tx, |tx| NodeRequest::GetTick { tx }).await;
         ok_json(format!("Tick: {}", tick))
       }
     });
 
     // == Blocks ==
 
-    let node_query_tx = node_query_sender.clone();
+    let query_tx = node_query_sender.clone();
     let get_blocks = path!("blocks").then(move || {
-      let node_query_tx = node_query_tx.clone();
+      let query_tx = query_tx.clone();
       async move {
-        let (tx, rx) = oneshot::channel();
-        node_query_tx.send(NodeRequest::GetBlocks { range: (-10, -1), tx }).unwrap();
-        let blocks = rx.await.unwrap();
+        let range = (-10, -1);
+        let blocks = ask(query_tx, |tx| NodeRequest::GetBlocks { range, tx }).await;
         ok_json(blocks)
       }
     });
 
     let get_block = || {
-      let node_query_tx = node_query_sender.clone();
+      let query_tx = node_query_sender.clone();
       path!("blocks" / String / ..).and_then(move |hash_hex: String| {
-        let node_query_tx = node_query_tx.clone();
+        let query_tx = query_tx.clone();
         async move {
-          let (tx, rx) = oneshot::channel();
           let hash_hex = hash_hex.strip_prefix("0x").unwrap_or(&hash_hex);
           match hex_to_u256(hash_hex) {
             Ok(hash) => {
-              node_query_tx.send(NodeRequest::GetBlock { hash, tx }).unwrap();
-              let block = rx.await.unwrap();
+              let block = ask(query_tx, |tx| NodeRequest::GetBlock { hash, tx }).await;
               Ok(block)
             }
             Err(err) => {
-              Err(reject::custom(InvalidParameter::from(format!("Invalid block hash: {}", err))))
+              Err(reject::custom(InvalidParameter::from(format!("Invalid block hash: '{}'", err))))
             }
           }
         }
@@ -161,40 +186,41 @@ pub fn api_loop(node_query_sender: SyncSender<NodeRequest>) {
 
     // == Functions ==
 
-    let node_query_tx = node_query_sender.clone();
+    let query_tx = node_query_sender.clone();
     let get_functions = path!("functions").then(move || {
-      let node_query_tx = node_query_tx.clone();
+      let query_tx = query_tx.clone();
       async move {
-        let (tx, rx) = oneshot::channel();
-        node_query_tx.send(NodeRequest::GetFunctions { tx }).unwrap();
-        let functions = rx.await.unwrap();
+        let functions = ask(query_tx, |tx| NodeRequest::GetFunctions { tx }).await;
         let functions: Vec<u128> = functions.into_iter().map(|x| x as u128).collect();
         let functions = u128_names_to_strings(&functions);
         ok_json(functions)
       }
     });
 
-    let node_query_tx = node_query_sender.clone();
-    let _get_function = path!("functions" / u128).then(move |id| {
-      let node_query_tx = node_query_tx.clone();
+    let get_function_base =
+      path!("functions" / String / ..).and_then(move |name_txt: String| async move {
+        if let Some(name) = name_to_u128_safe(&name_txt) {
+          Ok(name)
+        } else {
+          let msg = format!("Invalid function name: '{}'", name_txt);
+          Err(reject::custom(InvalidParameter::from(msg)))
+        }
+      });
+
+    let query_tx = node_query_sender.clone();
+    let _get_function = get_function_base.and(path!()).then(move |name: u128| {
+      let query_tx = query_tx.clone();
       async move {
-        let (tx, rx) = oneshot::channel();
-        node_query_tx.send(NodeRequest::GetFunction { name: id, tx }).unwrap();
-        let function = rx.await.unwrap();
+        let function = ask(query_tx, |tx| NodeRequest::GetFunction { name, tx }).await;
         ok_json(function)
       }
     });
 
-    // TODO merge code redundancy with above route
-    let node_query_tx = node_query_sender.clone();
-    let get_function_state = path!("functions" / String / "state").and_then(move |id: String| {
-      let node_query_tx = node_query_tx.clone();
+    let query_tx = node_query_sender.clone();
+    let get_function_state = get_function_base.and(path!("state")).and_then(move |name: u128| {
+      let query_tx = query_tx.clone();
       async move {
-        let (tx, rx) = oneshot::channel();
-        let node_query_tx = node_query_tx.clone();
-        let id = name_to_u128(&id);
-        node_query_tx.send(NodeRequest::GetState { name: id, tx }).unwrap();
-        let state = rx.await.unwrap();
+        let state = ask(query_tx, |tx| NodeRequest::GetState { name, tx }).await;
         if let Some(state) = state {
           Ok(ok_json(state))
         } else {
@@ -301,7 +327,7 @@ mod ser {
       let body_bytes = body.into_iter().collect::<Vec<_>>();
       let mut s = serializer.serialize_struct("Block", 4)?;
       s.serialize_field("time", &self.time.to_string())?;
-      s.serialize_field("rand", &self.rand.to_string())?;  // ?? hex?
+      s.serialize_field("rand", &self.rand.to_string())?; // ?? hex?
       s.serialize_field("prev", &u256_to_hex(&self.prev))?;
       s.serialize_field("body", &body_bytes)?;
       s.end()
