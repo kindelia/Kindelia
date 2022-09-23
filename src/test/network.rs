a78c12022e92da18cdad302469f584dd690022b9
rheidner <rheidner.achiles@gmail.com>
2022-09-23 16:20:27 -0300

    add graph initialization to network simulation

--- src/test/network.rs
+++ src/test/network.rs
@@ -1,3 +1,6 @@
+use petgraph::{
+  algo::astar, graph::NodeIndices, prelude::UnGraph, visit::IntoNodeIdentifiers,
+};
 use std::{
   collections::HashMap,
   path::PathBuf,
@@ -20,16 +23,17 @@ use super::util::temp_dir;
 #[test]
 #[ignore = "network simulation"]
 fn network() {
+  let mut g = UnGraph::new_undirected();
+  let a = g.add_node(0_u32);
+  let b = g.add_node(1);
+  let c = g.add_node(2);
+  g.extend_with_edges(&[
+    (a, b, RouterMockConnection::new(2, 0.5)),
+    (b, c, RouterMockConnection::new(5, 1.)),
+  ]);
+
   // creates the router
-  let mut router_mock = RouterMock::new();
-
-  // create three sockets mock
-  let socket_mock1 = router_mock.ask_sock();
-  let socket_mock2 = router_mock.ask_sock();
-  let socket_mock3 = router_mock.ask_sock();
-
-  let sockets = vec![socket_mock1, socket_mock2, socket_mock3];
-
+  let (router_mock, sockets) = RouterMock::from_graph(g.clone());
   let mut threads = vec![];
 
   // Spawns the router thread
@@ -41,8 +45,11 @@ fn network() {
   threads.push(router_thread);
 
   // Spawns the nodes threads
-
-  for socket in sockets {
+  for (socket, idx) in sockets {
+    let initial_peers = g
+      .neighbors(idx)
+      .map(|node| *g.node_weight(node).unwrap())
+      .collect::<Vec<_>>();
     let socket_thread = thread::spawn(move || {
       let state_path = temp_dir()
         .path
@@ -160,15 +167,66 @@ struct RouterMessage {
 /// - a `tx` `Sender` that is passed for the `sockets`
 /// - a `rx` `Receiver` that is capable of receiving `RouterMessage`'s from the all the sockets
 struct RouterMock {
+  network: UnGraph<u32, RouterMockConnection>,
   sockets: HashMap<u32, Sender<RouterMessage>>,
   rx: Receiver<RouterMessage>,
   tx: Sender<RouterMessage>,
 }
 
+#[derive(Default, Debug, PartialEq, Clone, Copy)]
+struct RouterMockConnection {
+  delay: u64,
+  error: f32,
+}
+
+impl RouterMockConnection {
+  fn new(delay: u64, error: f32) -> RouterMockConnection {
+    if error < 0. || error > 1. {
+      panic!("Only values between 0 and 1 are allowed for connection error");
+    }
+    RouterMockConnection { delay, error }
+  }
+}
+
+impl std::ops::Add for RouterMockConnection {
+  type Output = Self;
+  fn add(self, rhs: Self) -> Self::Output {
+    RouterMockConnection {
+      delay: self.delay + rhs.delay,
+      error: 1. - ((1. - self.error) * (1. - rhs.error)),
+    }
+  }
+}
+
+impl PartialOrd for RouterMockConnection {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    let error_cmp = self.error.partial_cmp(&other.error);
+    if let Some(std::cmp::Ordering::Equal) = error_cmp {
+      self.delay.partial_cmp(&other.delay)
+    } else {
+      error_cmp
+    }
+  }
+}
+
 impl RouterMock {
-  fn new() -> RouterMock {
-    let (tx, rx) = mpsc::channel();
-    RouterMock { sockets: HashMap::new(), rx, tx }
+  fn from_graph(
+    network: UnGraph<u32, RouterMockConnection>,
+  ) -> (RouterMock, Vec<(SocketMock, petgraph::prelude::NodeIndex)>) {
+    let (router_tx, router_rx) = mpsc::channel();
+
+    let mut sockets = HashMap::new();
+    let mut mocks = Vec::new();
+
+    for idx in network.node_indices() {
+      let addr = network.node_weight(idx).unwrap();
+      let (tx, rx) = mpsc::channel();
+      let socket_mock = SocketMock { addr: *addr, rx, tx: router_tx.clone() };
+      sockets.insert(*addr, tx);
+      mocks.push((socket_mock, idx))
+    }
+
+    (RouterMock { network, sockets, rx: router_rx, tx: router_tx }, mocks)
   }
 
   fn ask_sock(&mut self) -> SocketMock {
@@ -180,15 +238,44 @@ impl RouterMock {
     socket_mock
   }
 
+  fn connection(&self, addr1: u32, addr2: u32) -> Option<RouterMockConnection> {
+    astar(
+      &self.network,
+      addr1.into(),
+      |finish| finish == addr2.into(),
+      |e| *e.weight(),
+      |_| RouterMockConnection::default(),
+    )
+    .map(|res| res.0)
+  }
+
   fn send(&self, router_message: &RouterMessage) {
     let RouterMessage { to_addr, from_addr, msg } = router_message;
     let tx = self.sockets.get(&to_addr).unwrap();
-    tx.send(RouterMessage {
-      to_addr: *to_addr,
-      from_addr: *from_addr,
-      msg: msg.to_owned(),
-    })
-    .unwrap()
+
+    let connection = self.connection(*to_addr, *from_addr);
+    if let Some(connection) = connection {
+      let chance: f32 = rand::random();
+      // println!(
+      //   "Error chance of {} from {} to {}",
+      //   connection.error, from_addr, to_addr
+      // );
+      if chance > connection.error {
+        // println!(
+        //   "Awaiting {} from {} to {}",
+        //   connection.delay, from_addr, to_addr
+        // );
+        thread::sleep(std::time::Duration::from_millis(connection.delay));
+        tx.send(RouterMessage {
+          to_addr: *to_addr,
+          from_addr: *from_addr,
+          msg: msg.to_owned(),
+        })
+        .unwrap()
+      } else {
+        // println!("Not sending from {} to {}", from_addr, to_addr);
+      }
+    }
   }
 }
 
