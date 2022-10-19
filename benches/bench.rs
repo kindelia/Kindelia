dc94e92539b7a06a2919e5820f4797317776be39
Kelvin Steiner <me@steinerkelvin.dev>
2022-10-19 13:58:59 -0300

    fix bench `deserialize_block_with_transactions` +
    - `black_box`-ing data used in bench loops to avoid optimizations

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,7 +4,7 @@ extern crate test;
 // use rust_example::fib;
 
 // use kindelia::net::ProtoAddr;
-use test::Bencher;
+use test::{Bencher, black_box};
 
 use primitive_types::U256;
 
@@ -16,7 +16,7 @@ use kindelia::util;
 
 fn max_message() -> node::Message<net::Address> {
   let max_block = node::Block {
-    body: node::Body { data: [u8::MAX; node::MAX_BODY_SIZE].to_vec() },
+    body: node::Body { data: vec![u8::MAX; node::MAX_BODY_SIZE] },
     hash: U256::MAX,
     prev: U256::MAX,
     time: u128::MAX,
@@ -34,68 +34,65 @@ fn max_message() -> node::Message<net::Address> {
     seen_at: u128::MAX,
   };
 
-  let max_message = node::Message::NoticeTheseBlocks {
+  node::Message::NoticeTheseBlocks {
     magic: u64::MAX,
     gossip: true,
     blocks: vec![max_block],
-    peers: vec![max_peer.clone(), max_peer.clone(), max_peer.clone()],
-  };
-
-  max_message
+    peers: vec![max_peer, max_peer, max_peer],
+  }
 }
 
 #[bench]
 fn max_message_serialize(b: &mut Bencher) {
   let max_message = max_message();
-  b.iter(|| max_message.proto_serialized())
+  b.iter(|| {
+    let se_bits = black_box(&max_message).proto_serialized();
+    // util::bitvec_to_bytes(&se_bits)
+    se_bits
+  })
 }
 
 #[bench]
 fn max_message_deserialize(b: &mut Bencher) {
   let max_message = max_message();
-  let bits = max_message.proto_serialized();
-  b.iter(|| node::Message::<net::Address>::proto_deserialized(&bits).unwrap())
+  let se_bits = max_message.proto_serialized();
+  let se_bytes = util::bitvec_to_bytes(&se_bits);
+  b.iter(|| {
+    let se_bits = util::bytes_to_bitvec(black_box(&se_bytes));
+    node::Message::<net::Address>::proto_deserialized(&se_bits).unwrap()
+  })
 }
 
-// benchmark block serialization and extraction into transactions and statements
+/// Benchmarks deserialization and extraction of statements from a block
 #[bench]
-fn deserialize_block_with_transactions(b: &mut Bencher) {
-  // read code
-  let code = std::fs::read("./benches/codes/run_transactions.kdl").unwrap();
-  let code = String::from_utf8(code.to_vec()).unwrap();
+fn deserialize_block_with_txs(b: &mut Bencher) {
+  let code = include_str!("code/inc.kdl");
 
-  let (_, statement) = hvm::read_statement(&code).unwrap(); // create statement from code
-  let bytes = util::bitvec_to_bytes(&statement.proto_serialized()); // serialize it
-  let transaction = node::Transaction::new(bytes); // create transaction from it
+  let (_, base_stmt) = hvm::read_statement(code).unwrap();
+  let bytes = util::bitvec_to_bytes(&base_stmt.proto_serialized());
+  let transaction = node::Transaction::new(bytes);
 
-  // create body vector
-  let mut body_vec = vec![0];
-  // while body is not full
-  while body_vec.len() < node::MAX_BODY_SIZE {
-    // put the transaction in the body vector
-    // it will raise an error if the body is full
-    if let Err(_) =
-      node::build_body_from_transaction(&transaction, &mut body_vec)
-    {
-      break;
-    }
-  }
-  // create max block with the body
+  let body = node::Body::fill_from(std::iter::repeat(transaction));
+
   let block = node::Block {
-    body: node::Body { data: body_vec },
+    body,
     hash: U256::MAX,
     prev: U256::MAX,
     time: u128::MAX,
     meta: u128::MAX,
   };
-  let bits = block.proto_serialized(); // serialize it
+
+  let se_bits = block.proto_serialized();
+  let se_bytes = util::bitvec_to_bytes(&se_bits);
 
   b.iter(|| {
-    let block = node::Block::proto_deserialized(&bits).unwrap();
+    let de_bits = util::bytes_to_bitvec(black_box(&se_bytes));
+    let block = node::Block::proto_deserialized(&de_bits).unwrap();
     let transactions = node::extract_transactions(&block.body);
     for transaction in transactions {
-      let statement2 = transaction.to_statement().unwrap();
-      assert_eq!(statement, statement2)
+      let de_stmt = transaction.to_statement().unwrap();
+      debug_assert_eq!(base_stmt, de_stmt);
+      drop(de_stmt);
     }
   })
 }
