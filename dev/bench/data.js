8b58d18b7ea9b4ff00e942a89786183f05054189
github-action-benchmark <github@users.noreply.github.com>
2022-10-21 21:34:01 +0000

    add Simulation Benchmark (customSmallerIsBetter) benchmark result for 0c88c996a5b3abded9f483b91ff1fbe21ac69ad5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666388039842,
+  "lastUpdate": 1666388041886,
   "repoUrl": "https://github.com/Kindelia/Kindelia",
   "entries": {
     "Rust Benchmark": {
@@ -53,38 +53,33 @@ window.BENCHMARK_DATA = {
       "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5": {
         "commit": {
           "author": {
-            "email": "rheidner.achiles@gmail.com",
-            "name": "rheidner",
-            "username": "racs4"
+            "name": "Kindelia",
+            "username": "Kindelia"
           },
           "committer": {
-            "email": "rheidner.achiles@gmail.com",
-            "name": "rheidner",
-            "username": "racs4"
+            "name": "Kindelia",
+            "username": "Kindelia"
           },
-          "distinct": true,
           "id": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
-          "message": "testing gh-pages",
-          "timestamp": "2022-10-21T17:21:44-03:00",
-          "tree_id": "ecd2477a02a24a421027af227647d7ba1ea047d4",
-          "url": "https://github.com/Kindelia/Kindelia/commit/0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
-          "original_ref": "continuous-benchmark",
-          "parent": "35cbaa5b7d6acf4c67339f5111587043539388b1"
+          "message": "Adds github action for continuous benchmark",
+          "timestamp": "2022-10-20T10:53:24Z",
+          "url": "https://github.com/Kindelia/Kindelia/pull/197/commits/0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
+          "original_ref": "Kindelia:continuous-benchmark"
         },
-        "date": 1666384135606,
+        "date": 1666388041003,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
             "name": "Uncle Rate",
-            "value": 0.38636363636363635,
+            "value": 0.14583333333333331,
             "unit": "Percent",
-            "range": 0.16070608663330627
+            "range": 0.029462782549439473
           },
           {
             "name": "Failed Mining",
-            "value": 1108.5,
+            "value": 2046,
             "unit": "Logs",
-            "range": 997.7276682542185
+            "range": 1132.7850634608492
           }
         ]
       }
