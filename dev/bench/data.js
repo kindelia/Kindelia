937ed6a0a413d65a6cd9bdd44d1779536938466e
github-action-benchmark <github@users.noreply.github.com>
2022-10-21 21:33:59 +0000

    add Rust Benchmark (cargo) benchmark result for 0c88c996a5b3abded9f483b91ff1fbe21ac69ad5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,48 +1,49 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666384135945,
+  "lastUpdate": 1666388039842,
   "repoUrl": "https://github.com/Kindelia/Kindelia",
   "entries": {
     "Rust Benchmark": {
       "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5": {
         "commit": {
           "author": {
-            "email": "rheidner.achiles@gmail.com",
-            "name": "rheidner",
-            "username": "racs4"
+            "name": "Kindelia",
+            "username": "Kindelia"
           },
           "committer": {
-            "email": "rheidner.achiles@gmail.com",
-            "name": "rheidner",
-            "username": "racs4"
+            "name": "Kindelia",
+            "username": "Kindelia"
           },
-          "distinct": true,
           "id": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
-          "message": "testing gh-pages",
-          "timestamp": "2022-10-21T17:21:44-03:00",
-          "tree_id": "ecd2477a02a24a421027af227647d7ba1ea047d4",
-          "url": "https://github.com/Kindelia/Kindelia/commit/0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
-          "original_ref": "continuous-benchmark",
-          "parent": "35cbaa5b7d6acf4c67339f5111587043539388b1"
+          "message": "Adds github action for continuous benchmark",
+          "timestamp": "2022-10-20T10:53:24Z",
+          "url": "https://github.com/Kindelia/Kindelia/pull/197/commits/0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
+          "original_ref": "Kindelia:continuous-benchmark"
         },
-        "date": 1666384134301,
+        "date": 1666388038874,
         "tool": "cargo",
         "benches": [
           {
+            "name": "kvm_tree_sum",
+            "value": 2668832,
+            "range": "± 81246",
+            "unit": "ns/iter"
+          },
+          {
             "name": "max_message_serialize",
-            "value": 32471,
-            "range": "± 1562",
+            "value": 39388,
+            "range": "± 32",
             "unit": "ns/iter"
           },
           {
             "name": "max_message_deserialize",
-            "value": 39677,
-            "range": "± 61",
+            "value": 47662,
+            "range": "± 60",
             "unit": "ns/iter"
           },
           {
             "name": "deserialize_block_with_txs",
-            "value": 159280,
-            "range": "± 2749",
+            "value": 190660,
+            "range": "± 846",
             "unit": "ns/iter"
           }
         ]
@@ -90,6 +91,7 @@ window.BENCHMARK_DATA = {
     }
   },
   "branches": {
-    "continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5"
+    "continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
+    "Kindelia:continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5"
   }
 }
