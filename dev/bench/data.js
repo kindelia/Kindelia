f2b105ca6ba4cff9dc969544e2ff2ebc17cba781
github-action-benchmark <github@users.noreply.github.com>
2022-10-25 13:44:04 +0000

    add Rust Benchmark (cargo) benchmark result for 7c8b1c8fb4224d098eaac3ec0f3c3a80329ba4e7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666388041886,
+  "lastUpdate": 1666705444674,
   "repoUrl": "https://github.com/Kindelia/Kindelia",
   "entries": {
     "Rust Benchmark": {
@@ -47,6 +47,56 @@ window.BENCHMARK_DATA = {
             "unit": "ns/iter"
           }
         ]
+      },
+      "7c8b1c8fb4224d098eaac3ec0f3c3a80329ba4e7": {
+        "commit": {
+          "author": {
+            "email": "me@steinerkelvin.dev",
+            "name": "Kelvin Steiner",
+            "username": "steinerkelvin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7c8b1c8fb4224d098eaac3ec0f3c3a80329ba4e7",
+          "message": "Merge pull request #197 from Kindelia/continuous-benchmark\n\nAdds github action for continuous benchmark",
+          "timestamp": "2022-10-25T10:29:06-03:00",
+          "tree_id": "72c44257586252a64fdc8ba58b5a1d6f032f334e",
+          "url": "https://github.com/Kindelia/Kindelia/commit/7c8b1c8fb4224d098eaac3ec0f3c3a80329ba4e7",
+          "original_ref": "dev",
+          "parent": "8f80194bf99bd8852d46c090683cb5fac98946a2"
+        },
+        "date": 1666705443745,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "kvm_tree_sum",
+            "value": 2812756,
+            "range": "± 95721",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "max_message_serialize",
+            "value": 43158,
+            "range": "± 1419",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "max_message_deserialize",
+            "value": 45095,
+            "range": "± 2688",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "deserialize_block_with_txs",
+            "value": 193876,
+            "range": "± 6493",
+            "unit": "ns/iter"
+          }
+        ]
       }
     },
     "Simulation Benchmark": {
@@ -87,6 +137,7 @@ window.BENCHMARK_DATA = {
   },
   "branches": {
     "continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
-    "Kindelia:continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5"
+    "Kindelia:continuous-benchmark": "0c88c996a5b3abded9f483b91ff1fbe21ac69ad5",
+    "dev": "7c8b1c8fb4224d098eaac3ec0f3c3a80329ba4e7"
   }
 }
